666803bba59a1ca6364f78902cdf2f730924a4f8
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-10-17 00:19:09 -0300

    feat: Adicionar botão de busca

--- src/App.js
+++ src/App.js
@@ -53,6 +53,19 @@ class App extends Component {
                   </button>
                 </div>
               </div>
+              <div className='collapse navbar-collapse justify-content-end' id='navigation'>
+                <ul className='navbar-nav'>
+                  <li className='nav-item'>
+                    <a className='nav-link btn-rotate' href='#'>
+                      <p className='search-button'>Search</p>
+                      <i className='nc-icon nc-simple-add'></i>
+                      <p>
+                        <span className='d-lg-none d-md-block'>Add</span>
+                      </p>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </nav>
         </div>
