be26fdb869dc0d90cdf11099123f14eb10162bab
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-10-17 23:24:50 -0300

    refactor: Refatorar exibição das opções dos livros
    Ajustada a opção escolhida pelo usuário. Foi refatorado para que isso
    seja gerenciado por state, deixando o React responsável pela reação.

--- src/Book.js
+++ src/Book.js
@@ -1,55 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BookOptions from './BookOptions';
 import BookShare from './BookShare';
 
-const Book = (props) => (
-    <div className='book'>
-      <div className='book__image'>
-        <div
-          className='book__share book__option'
-          onClick={(e) => {
-            toggleBookOption(e, 'sharing');
-          }}>
-          <i className="nc-icon nc-share-66"></i>
-        </div>
-        <BookShare/>
-        <BookOptions/>
-        <img
-          src='http://books.google.com/books/content?id=mDzDBQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
-          alt='Eloquent JavaScript, 2nd Ed.'
-        />
-        <div
-          className='book__set book__option'
-          onClick={(e) => {
-            toggleBookOption(e, 'options');
-          }}>
-          <i className="nc-icon nc-settings-gear-65"></i>
-        </div>
-      </div>
-      <div className='book__description'>
-      Eloquent JavaScript, 2nd Ed.
-      </div>
-    </div>
-)
+class Book extends Component {
 
-const toggleBookOption = (e, type) => {
-  const ACTIVE_CLASS_NAME = `book__${type}--active`;
-  const element = e.target;
-  const bookElement = element.closest('.book');
-
-  if(!bookElement) {
-    return;
+  constructor() {
+    super();
+    this.state = {
+      showBookShare: false,
+      showBookOptions: false
+    };
+    this.initialState = this.state;
   }
 
-  const bookOptions = bookElement.querySelector(`.book__${type}`);
-
-  if(!bookOptions) {
-    return;
+  render() {
+    return (
+      <div className='book'>
+        <div className='book__image'>
+          <div
+            className='book__share book__option'
+            onClick={(e) => {
+              return this.toggleBookChoice(e, 'showBookShare');
+            }}>
+            <i className="nc-icon nc-share-66"></i>
+          </div>
+          <BookShare show={this.state.showBookShare}/>
+          <BookOptions show={this.state.showBookOptions}/>
+          <img
+            src='http://books.google.com/books/content?id=mDzDBQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+            alt='Eloquent JavaScript, 2nd Ed.'
+          />
+          <div
+            className='book__set book__option'
+            onClick={(e) => {
+              return this.toggleBookChoice(e, 'showBookOptions');
+            }}>
+            <i className="nc-icon nc-settings-gear-65"></i>
+          </div>
+        </div>
+        <div className='book__description'>
+        Eloquent JavaScript, 2nd Ed.
+        </div>
+      </div>
+    );
   }
 
-  bookOptions.classList.contains(ACTIVE_CLASS_NAME)
-    ? bookOptions.classList.remove(ACTIVE_CLASS_NAME)
-    : bookOptions.classList.add(ACTIVE_CLASS_NAME);
+  toggleBookChoice = (e, choice) => {
+    this.setState((prevState) => ({
+      ...this.initialState,
+      [choice]: !this.state[choice]
+    }));
+  }
 }
 
 export default Book;
--- src/BookOptions.js
+++ src/BookOptions.js
@@ -1,53 +1,59 @@
 import React from 'react';
 
-const BookOptions = (props) => (
-  <div className='book__options'>
-    <h6 className='text-center book__label'>Status</h6>
-    <form className='book__form'>
-      <div className='book__status-option'>
-        <input
-          className='book__value'
-          type='radio'
-          name='bookStatus'
-          id='currentlyReading'
-          value='currentlyReading'
-        />
-        <label
-          className='book__status'
-          htmlFor='currentlyReading'>
-            Currently reading
-        </label>
-      </div>
-      <div className='book__status-option'>
-        <input
-          className='book__value'
-          type='radio'
-          name='bookStatus'
-          id='wantToRead'
-          value='wantToRead'
-        />
-        <label
-          className='book__status'
-          htmlFor='wantToRead'>
-            Want to read
-        </label>
-      </div>
-      <div className='book__status-option'>
-        <input
-          className='book__value'
-          type='radio'
-          name='bookStatus'
-          id='read'
-          value='read'
-        />
-        <label
-          className='book__status'
-          htmlFor='read'>
-            Read
-        </label>
-      </div>
-    </form>
-  </div>
-);
+const BookOptions = (props) => {
+  const activeClass = props.show
+    ? 'book__options--active'
+    : '';
+
+  return (
+    <div className={`book__options ${activeClass}`}>
+      <h6 className='text-center book__label'>Status</h6>
+      <form className='book__form'>
+        <div className='book__status-option'>
+          <input
+            className='book__value'
+            type='radio'
+            name='bookStatus'
+            id='currentlyReading'
+            value='currentlyReading'
+          />
+          <label
+            className='book__status'
+            htmlFor='currentlyReading'>
+              Currently reading
+          </label>
+        </div>
+        <div className='book__status-option'>
+          <input
+            className='book__value'
+            type='radio'
+            name='bookStatus'
+            id='wantToRead'
+            value='wantToRead'
+          />
+          <label
+            className='book__status'
+            htmlFor='wantToRead'>
+              Want to read
+          </label>
+        </div>
+        <div className='book__status-option'>
+          <input
+            className='book__value'
+            type='radio'
+            name='bookStatus'
+            id='read'
+            value='read'
+          />
+          <label
+            className='book__status'
+            htmlFor='read'>
+              Read
+          </label>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 export default BookOptions;
