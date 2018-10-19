c2a938a6332e0351371cb499ade77dc7741ba3ae
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-10-18 21:56:42 -0300

    refactor: Mudar NavbarWrapper de componente
    Inserido NavbarWrapper dentro do componente NavbarButtons.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import NavbarButtons from './NavbarButtons';
-import NavbarWrapper from './NavbarWrapper';
 
 const Navbar = () => (
   <nav className='navbar navbar-expand-lg navbar-absolute fixed-top'>
     <div className='container-fluid'>
-      <NavbarWrapper/>
       <NavbarButtons/>
     </div>
   </nav>
--- src/NavbarButtons.js
+++ src/NavbarButtons.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import LinkButton from './LinkButton';
+import NavbarWrapper from './NavbarWrapper';
 
 const NavbarButtons = (props) => (
-  <div className='collapse navbar-collapse justify-content-end' id='navigation'>
-    <ul className='navbar-nav'>
-      <li className='nav-item'>
-        <Route
-          exact
-          path='/'
-          render={() => <LinkButton label='Search' link='/search'/>}
-        />
-        <Route
-          path='/search'
-          render={() => <LinkButton label='Back to Home' link='/'/>}
-        />
-      </li>
-    </ul>
-  </div>
+  <React.Fragment>
+    <NavbarWrapper/>
+    <div className='collapse navbar-collapse justify-content-end' id='navigation'>
+      <ul className='navbar-nav'>
+        <li className='nav-item'>
+          <Route
+            exact
+            path='/'
+            render={() => <LinkButton label='Search' link='/search'/>}
+          />
+          <Route
+            path='/search'
+            render={() => <LinkButton label='Back to Home' link='/'/>}
+          />
+        </li>
+      </ul>
+    </div>
+  </React.Fragment>
 );
 
 export default NavbarButtons;
