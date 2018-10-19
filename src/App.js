95055c90f4504d52890c579fe4d39683e1a289de
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-10-19 20:52:14 -0300

    feat: Inserir condição para exibir filtro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import MainPanel from './MainPanel';
 import './Style.css';
 
-const App = (props) => (
+const App = () => (
     <div className='wrapper'>
-      <Sidebar/>
+      <Route path='/' component={Sidebar}/>
       <MainPanel/>
     </div>
 )
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Filter from './Filter';
 import { Link } from 'react-router-dom';
 
-const Sidebar = () => (
+const Sidebar = (props) => (
   <div className='sidebar' data-color='black' data-active-color='danger'>
     <div className='logo text-center'>
       <Link to='/' className='simple-text'>
@@ -10,7 +10,8 @@ const Sidebar = () => (
       </Link>
     </div>
     <div className='sidebar-wrapper'>
-      <Filter/>
+      {/* Apenas irá apresentar o filter se estiver fora da página /search */}
+      {props.location.pathname !== '/search' && <Filter/>}
     </div>
   </div>
 );
