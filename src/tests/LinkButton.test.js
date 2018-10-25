2a530cf6a4d72d957f8b53f805cc7a3276acd9b1
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-10-24 22:43:22 -0300

    test: Escrever testes para LinkButton

---
+++ src/tests/LinkButton.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LinkButton from '../LinkButton';
+
+describe('<LinkButton/>', () => {
+  const props = {
+    location: {
+      pathname: '/search'
+    }
+  }
+
+  it('shallow renders correctly', () => {
+    expect(shallow(<LinkButton {...props}/>));
+  });
+
+  it('should have a Link element', () => {
+    const wrapper = shallow(<LinkButton {...props}/>);
+    expect(wrapper.find('Link').length).toBe(1);
+  });
+});
