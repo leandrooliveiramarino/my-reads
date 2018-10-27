6cad808897f3410a895042c73d79706f91a23392
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-10-27 19:27:24 -0300

    refactor: Melhorar legibilidade

--- src/Shelves.js
+++ src/Shelves.js
@@ -2,70 +2,59 @@ import React from 'react';
 import Shelf from './Shelf';
 import { Route } from 'react-router-dom';
 
-const Shelves = (props) => (
+const shelves = [
+  {
+    title: 'Currently reading',
+    status: 'currentlyReading',
+    showHr: true,
+    path: '/currently-reading'
+  },
+  {
+    title: 'Want to read',
+    status: 'wantToRead',
+    showHr: true,
+    path: '/want-to-read'
+  },
+  {
+    title: 'Read',
+    status: 'read',
+    showHr: false,
+    path: '/read'
+  }
+]
+
+const Shelves = props => (
   <div className='row justify-content-md-center'>
     <div className='col-sm-12 text-center'>
+      {/* Quando estamos na home, mostramos todas as prateleiras*/}
       <Route exact path='/' render={() => (
-        <React.Fragment>
-          {
-            <Shelf
-              title='Currently reading'
-              status='currentlyReading'
-              showHr={true}
-              books={props.currentlyReading}
-              onChangeBookChoice={props.onChangeBookChoice}
-            />
-          }
-          {
-            <Shelf
-              title='Want to Read'
-              status='wantToRead'
-              showHr={true}
-              books={props.wantToRead}
-              onChangeBookChoice={props.onChangeBookChoice}
-            />
-          }
-          {
-            <Shelf
-              title='Read'
-              status='read'
-              showHr={false}
-              books={props.read}
-              onChangeBookChoice={props.onChangeBookChoice}
-            />
-          }
-        </React.Fragment>
+        shelves.map(shelf => (
+          <Shelf
+            key={shelf.status}
+            title={shelf.title}
+            status={shelf.status}
+            showHr={shelf.showHr}
+            books={props[shelf.status]}
+            onChangeBookChoice={props.onChangeBookChoice}
+          />
+        ))
       )}/>
 
-      <Route path='/currently-reading' render={() => (
-        <Shelf
-          title='Currently reading'
-          status='currentlyReading'
-          showHr={false}
-          books={props.currentlyReading}
-          onChangeBookChoice={props.onChangeBookChoice}
+      {shelves.map(shelf => (
+        <Route
+          key={shelf.status}
+          path={shelf.path}
+          render={() => (
+            <Shelf
+              title={shelf.title}
+              status={shelf.status}
+              showHr={shelf.showHr}
+              books={props[shelf.status]}
+              onChangeBookChoice={props.onChangeBookChoice}
+            />
+          )}
         />
-      )}/>
-
-      <Route path='/want-to-read' render={() => (
-        <Shelf
-          title='Want to read'
-          status='wantToRead'
-          showHr={false}
-          books={props.wantToRead}
-          onChangeBookChoice={props.onChangeBookChoice}
-        />
-      )}/>
-
-      <Route path='/read' render={() => (
-        <Shelf
-          title='Read'
-          status='read'
-          showHr={false}
-          books={props.read}
-          onChangeBookChoice={props.onChangeBookChoice}
-        />
-      )}/>
+      ))}
     </div>
   </div>
 );
